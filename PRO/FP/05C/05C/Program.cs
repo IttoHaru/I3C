a2abbd34c878fb836ccc8c37cdaa107d2f25f111
IttoHaru <102925007+IttoHaru@users.noreply.github.com>
2022-10-20 14:38:24 +0200

    Add files via upload

---
+++ PRO/FP/05C/05C/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("big eyed toe nail crap mutant"); //output on dysplay/console
+
+            /*
+            Console.WriteLine("big eyed toe nail crap mutant");
+            */
+
+            //Console.WriteLine("big eyed toe nail crap mutant");
+            //Console.WriteLine("big eyed toe nail crap mutant");
+            //Console.WriteLine("big eyed toe nail crap mutant");
+        }
+    }
+}
---
+++ PRO/FP/06C/06C/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double k, l, m; //input
+
+            Console.WriteLine("vzorec je K = 2L + M");
+
+            Console.WriteLine("zadejte číslo L"); //number l
+            l = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("zadejte číslo M"); //number m
+            m = double.Parse(Console.ReadLine());
+
+            k = 2 * l + m; //calculation
+
+            Console.WriteLine("K = " + k); //output
+
+            Console.ReadLine();
+        }
+    }
+}
