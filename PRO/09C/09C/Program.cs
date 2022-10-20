59c9cd9ddadf8ae320eb231cc4e7db8ecc4a7e11
IttoHaru <102925007+IttoHaru@users.noreply.github.com>
2022-10-20 14:00:33 +0200

    Add files via upload

---
+++ PRO/09C/09C/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _09C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej celé číslo: ");
+            int a = int.Parse(Console.ReadLine());
+
+            //lze také:
+            //int a;                                        //deklarace proměnné
+            //a = Convert.ToInt32(Console.ReadLine());      //příkaz pro vstup
+
+            Console.Write("Zadej desetinné číslo: ");
+            float b = float.Parse(Console.ReadLine());
+
+            Console.Write("Zadej desetinné číslo: ");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.Write("Zadej logickou hodnotu: ");     //true nebo false
+            bool d = bool.Parse(Console.ReadLine());
+
+            Console.Write("Zadej znak: ");
+            char e = char.Parse(Console.ReadLine());
+
+            Console.Write("Zadej řetězec znaků: ");
+            string f = Console.ReadLine();                  //neprovádí se konverze hodnoty
+
+            Console.WriteLine();
+
+            Console.WriteLine("Byly zadány tyto hodnoty:");
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ PRO/10C/10C/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double a, o;                                            //deklarace proměnných
+            
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("PROGRAM PRO V7POČET");
+            Console.WriteLine("--------------------------------");
+
+            Console.Write("zadej stranu čtverce a v cm: ");
+            a = double.Parse(Console.ReadLine());                   //vztup
+
+            o = 4 * a;                                              //zpracování
+
+            Console.WriteLine("Obvod čtverce je " + o + " cm.");    //výstup
+
+            Console.ReadLine();
+        }
+    }
+}
