b4b13d97c0deafbd009babc53b4d81dc7e5cebc5
IttoHaru <102925007+IttoHaru@users.noreply.github.com>
2022-10-21 10:39:54 +0200

    výpočet obdelníku

---
+++ PRO/FP/11C/11C/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _11C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("obvod obdelníka");
+            
+            Console.WriteLine("-------------------------");
+
+            double a, b, o;
+            
+            Console.WriteLine("zadejte hodnoty v cm");
+
+            Console.Write("hodnota a: ");
+            a = Double.Parse(Console.ReadLine());
+
+            Console.Write("hodnota b: ");
+            b = Double.Parse(Console.ReadLine());
+
+            o = 2 * (a + b);
+
+            Console.WriteLine("-------------------------");
+            
+            Console.WriteLine("obvod je: " + o);
+
+            Console.ReadLine();
+        }
+    }
+}
